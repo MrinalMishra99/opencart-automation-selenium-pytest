1b9f1bd356f0e769c25ab53a81658a48a158efd6
Mrinal Mishra <mrinal.mishra@sagarsoft.in>
2023-05-09 12:26:28 +0530

    "Made basic changes in the code"

---
+++ pageObjects/LoginPage.py
@@ -0,0 +1,3 @@
+class LoginPage():
+    txt_email_xpath = '//*[@id="input-email"]'
+    txt_pass_xpath = '//*[@id="input-password"]'
--- testCases/conftest.py
+++ testCases/conftest.py
@@ -35,7 +35,7 @@ def pytest_configure(config):
 
 
 # It is hook for delete/Modify Environment info to HTML Report
-@pytest.mark.optionalhook
+@pytest.hookimpl(optionalhook=True)
 def pytest_metadata(metadata):
     metadata.pop("JAVA_HOME", None)
     metadata.pop("Plugins", None)
